8486a70b212fc938fbf79203e857ac0b32404384
Jameskiel518@gmail.com <>
2021-09-13 23:02:22 -0400

    Second Commit: added int ID,Code and String Name and getters and setters for each. Also fixed import scanner by moving it below package

--- CSC229_Lab1/src/main/java/com/mycompany/csc229_lab1/Course.java
+++ CSC229_Lab1/src/main/java/com/mycompany/csc229_lab1/Course.java
@@ -1,17 +1,38 @@
+package com.mycompany.CSC229_Lab1;
 import java.util.Scanner;
-package com.mycompany.csc229_lab1;
-
 
 public class Course {
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    int ID, Code;
+    String Name;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getCode() {
+        return Code;
+    }
+
+    public void setCode(int Code) {
+        this.Code = Code;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+      
+    public static void main(String args[]){
+        
+    }
+
     
 }
