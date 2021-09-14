fbc6720609f690abc21739cf0ec508eaa693f50b
Jameskiel518@gmail.com <djkaz@LAPTOP-BVVVLGK4.fios-router.home>
2021-09-14 00:04:29 -0400

    Fourth Commit: Added output statements to have the user change the member variables using the methods in class "Course". It then outputs those values back to the user.

--- CSC229_Lab1/src/main/java/com/mycompany/csc229_lab1/Course.java
+++ CSC229_Lab1/src/main/java/com/mycompany/csc229_lab1/Course.java
@@ -1,23 +1,21 @@
 package com.mycompany.CSC229_Lab1;
 import java.util.Scanner;
 
-public class Course {
+ class Course {
+    int ID, Code;
+    String Name;
 
-  //Overloaded Constructor for changing member variables 
+//Default Constructor 
+    public Course() {
+    }
+    
+//Overloaded Constructor for changing member variables 
     public Course(int ID, int Code, String Name) {
         this.ID = ID;
         this.Code = Code;
         this.Name = Name;
     }
-
-  //Default Constructor 
-    public Course() {
-    }
     
-    
-    int ID, Code;
-    String Name;
-
     public int getID() {
         return ID;
     }
@@ -43,12 +41,32 @@ public class Course {
     }
       
     public static void main(String args[]){
-        
+       
+//Instantiating Course class and assigning default values for members
         Course Course1;
+        Course1 = new Course(00,0000,"Default");
+       
+//Collecting user input for "Code" member variable
         
+        System.out.print("Input user's Code #");
+        Scanner Codescnr = new Scanner(System.in);
+        Course1.setCode(Codescnr.nextInt());
         
+//Collecting user input for "ID" member variable        
+        System.out.print("Input user's ID #");
+        Scanner IDscnr = new Scanner(System.in);
+        Course1.setID(IDscnr.nextInt());
         
+//Collecting user input for "Name" member variable
+        System.out.print("Input user's Name");
+        Scanner Namescnr = new Scanner(System.in);
+        Course1.setName(Namescnr.nextLine());
+        
+//Outputting user input for all member variables        
+        System.out.println("Users Code # is " + Course1.getCode());
+        System.out.println("Users ID is " + Course1.getID());
+        System.out.println("Users Name is " + Course1.getName());
+  
     }
-
     
 }
